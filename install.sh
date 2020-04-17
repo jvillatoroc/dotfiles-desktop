869fce6e2f572d5e0223c7c8d62af69ac7ca6990
Jonathan Villatoro <lacho8713@gmail.com>
2020-04-16 23:00:57 -0600

    expanded installer functionality

--- install.sh
+++ install.sh
@@ -22,20 +22,62 @@ pkg_upgrade
 
 pkg_install vim curl git zsh
 
-mkdir ~/Repos && cd ~/Repos
-git clone https://github.com/jvillatoroc/dotfiles-laptop.git
-
 case "$PKG_MGR" in
 	pacman)
 		pkg_install xorg-server xorg-apps
-		pkg_install xorg-video-intel nvidia
+		pkg_install xorg-video-intel nvidia xorg-xinit
 		pkg_install ttf-dejavu ttf-liberation ttf-inconsolata
 		;;
 	apt)
-		pkg_install xorg
+		pkg_install xorg xinit
+		pkg_install nvidia-detect nvidia-legacy-390xx-driver
 		;;
 esac
 
+pkg_install gnupg feh
+
+# Setup Repos directory
+REPDIR=~/Repos
+mkdir $REPDIR && cd $REPDIR
+
+# Clone my dotfiles
+git clone https://github.com/jvillatoroc/dotfiles-laptop.git
+cd dotfiles-laptop
+echo "dotfiles cloned successfully"
+
+# Install my dwm build
+git clone https://github.com/jvillatoroc/dwm.git
+cd dwm
+make clean && make && sudo make install
+cd ..
+
+# Install my st build
+git clone https://github.com/jvillatoroc/st.git
+cd st
+make clean && make && sudo make install
+cd ..
+
+# Install required programs
+pkg_install neomutt irssi mpd ncmpcpp calcurse newsboat
+
+# Create symbolic links
+cd ~
+rm .bashrc
+ln -s $REPDIR/dotfiles-laptop/.bash_profile
+ln -s $REPDIR/dotfiles-laptop/.calcurse
+ln -s $REPDIR/dotfiles-laptop/.config
+ln -s $REPDIR/dotfiles-laptop/.fehbg
+ln -s $REPDIR/dotfiles-laptop/.irssi
+ln -s $REPDIR/dotfiles-laptop/.local
+ln -s $REPDIR/dotfiles-laptop/.ncmpcpp
+ln -s $REPDIR/dotfiles-laptop/.newsboat
+ln -s $REPDIR/dotfiles-laptop/.oh-my-zsh
+ln -s $REPDIR/dotfiles-laptop/.vim
+ln -s $REPDIR/dotfiles-laptop/.vimrc
+ln -s $REPDIR/dotfiles-laptop/.xinitrc
+ln -s $REPDIR/dotfiles-laptop/.Xresources
+ln -s $REPDIR/dotfiles-laptop/.xprofile
+ln -s $REPDIR/dotfiles-laptop/.zprofile
+ln -s $REPDIR/dotfiles-laptop/.zshrc
 
 
-pkg_install gnupg neomutt irssi mpd ncmpcpp calcurse
